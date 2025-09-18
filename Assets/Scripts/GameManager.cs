a5c28a3a95794fea000ccee358d0c5a7d2844ed9
Shingugitvr000 <Shingugitvr000@gmail.com>
2025-09-18 11:39:19 +0900

    [Feat] 애니메이션 점프, 공격 , GameManager 제작

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("게임 상태")]
+    public int playerScore = 0;
+    public int itemsColledted = 0;
+
+    [Header("UI 참조")]
+    public Text scoreText;
+    public Text itemCountText;
+    public Text gameStatusText;
+
+    public static GameManager Instance;                         //싱글턴 패턴
+
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void CollectItem()
+    {
+        itemsColledted++;
+        Debug.Log($"아이템 수집! (총 : {itemsColledted} 개");
+    }
+
+    public void UpdateUI()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "접수 : " + playerScore;
+        }
+
+        if (itemCountText != null)
+        {
+            itemCountText.text = "아이템 : " + itemsColledted + "개";
+        }
+    }
+        
+
+}
+
+
+
