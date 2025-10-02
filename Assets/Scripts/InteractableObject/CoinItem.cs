26100950a1a5613287b809e8a1849ba8b4f160aa
Shingugitvr000 <Shingugitvr000@gmail.com>
2025-10-02 11:42:34 +0900

    [Feat] 이동, 수집 퀘스트 완료

---
+++ Assets/Scripts/InteractableObject/CoinItem.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinItem : InteractableObject
+{
+    [Header("동전 설정")]
+    public int coinValue = 10;
+    public string questTag = "Coin";                                //퀘스트에서 사용할 태그
+
+    // Start is called before the first frame update
+    protected override void Start()  
+    {
+        base.Start();
+        objectName = "동전";
+        interactionText = "[E] 동전 획득";
+        interactionType = InteractionType.Item;
+    }
+
+    protected override void CollectItem()
+    {
+        if(QuestManager.Instance != null)
+        {
+            QuestManager.Instance.AddCollectProgress(questTag);
+        }
+        Destroy(gameObject);
+    }
+
+}
