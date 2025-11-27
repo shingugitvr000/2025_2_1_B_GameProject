5b7dd77dcb6fcbccb1c1857a5b6d0267f5c0fff6
Shingugitvr000 <Shingugitvr000@gmail.com>
2025-11-27 10:35:08 +0900

    [Feat] Effect Manager 제작

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -37,6 +37,8 @@ public class PlayerController : MonoBehaviour
 
     private bool isUIMode = false;                                  //UI 모드 설정
 
+    public GameObject effectPos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -140,6 +142,8 @@ public class PlayerController : MonoBehaviour
             {
                 isLanding = true;
                 landingTimer = landingDuration;
+
+                EffectManager.Instance.PlayEffect("착지이펙트", transform.position);
             }
         }
     }
@@ -199,6 +203,7 @@ public class PlayerController : MonoBehaviour
             if (animator != null)
             {
                 animator.SetTrigger("attackTrigger");
+                EffectManager.Instance.PlayEffectWithDelay("공격이펙트", effectPos.transform.position, Quaternion.identity, 0.5f, 1.0f);
             }
         }
     }
